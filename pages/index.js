153c8508f379b9fa583d8fcddc8906f8c4400781
Kristopher Ong <k.ong@arcanys.com>
2023-05-12 16:05:51 +0800

    ✨feature: test deployment preview

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Learner of Anything</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          This blog is created using Next.js 👨‍💻
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
